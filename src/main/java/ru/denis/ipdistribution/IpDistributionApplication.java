26e97089923e8d37227118987e36136538bc4f45
FrozenMorozen <morozovd96@outlook.com>
2020-03-02 00:52:02 +0700

    Initial commit

---
+++ src/main/java/ru/denis/ipdistribution/IpDistributionApplication.java
@@ -0,0 +1,13 @@
+package ru.denis.ipdistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpDistributionApplication.class, args);
+	}
+
+}
